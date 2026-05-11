89cab8fcc7503190b7ae338d06ae32f8d99096d4
juanmora-ux <juanpablomorasilva@gmail.com>
2026-05-10 19:34:18 -0500

    Update CrossOrigin annotation in ProductController

--- marketplace/marketplace/src/main/java/com/unisabana/marketplace/marketplace/controller/ProductController.java
+++ marketplace/marketplace/src/main/java/com/unisabana/marketplace/marketplace/controller/ProductController.java
@@ -18,7 +18,7 @@ import com.unisabana.marketplace.marketplace.repository.DataStore;
 
 @RestController
 @RequestMapping("/api/v1/products")
-@CrossOrigin(origins = "https://unisabana-marketplace-frontend.vercel.app, http://localhost:5175") 
+@CrossOrigin(origins = "https://unisabana-marketplace-frontend.vercel.app") 
 public class ProductController {
 
     private final DataStore dataStore;
