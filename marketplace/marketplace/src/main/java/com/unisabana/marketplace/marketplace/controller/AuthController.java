e955984cd94044df498e6fdd14bf4fe4f7fb2de0
juanmora-ux <juanpablomorasilva@gmail.com>
2026-05-10 19:33:52 -0500

    Update CrossOrigin configuration in AuthController

--- marketplace/marketplace/src/main/java/com/unisabana/marketplace/marketplace/controller/AuthController.java
+++ marketplace/marketplace/src/main/java/com/unisabana/marketplace/marketplace/controller/AuthController.java
@@ -11,7 +11,7 @@ import com.unisabana.marketplace.marketplace.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "https://unisabana-marketplace-frontend.vercel.app , http://localhost:5175") //  React pueda conectarse
+@CrossOrigin(origins = "https://unisabana-marketplace-frontend.vercel.app") //  React pueda conectarse
 public class AuthController {
 
     private final AuthService authService;
